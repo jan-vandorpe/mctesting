0a29552ad9d392f1f3418beb6f26ec04d3a8d419
Thomas Deserranno <thomas.deserranno@gmail.com>
2014-03-28 09:42:41 +0100

    works ....

---
+++ src/Framework/AbstractController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Framework;
+/**
+ * Description of controller
+ * 
+ * Framework base controller class containing the application object
+ * 
+ * All controllers created in an application need to extend this class to have
+ * access to the class loaders and twig environment.
+ *
+ * @author Thomas
+ */
+abstract class AbstractController
+{
+    protected $app;
+    
+    function __construct($app)
+    {
+        $this->app = $app;
+    }
+    
+    public function render($view, $model)
+    {
+        print($this->app->getAppEnvironment()->render($view, $model));
+    }
+}
