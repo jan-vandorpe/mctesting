478c7df928ef5e7eeed95e235a4b572129c61d17
Bmortier <bert_mortier@hotmail.com>
2014-04-09 08:34:12 +0200

    category ent dao serv2

--- src/Mctesting/Model/Data/SubcategoryDAO.php
+++ src/Mctesting/Model/Data/SubcategoryDAO.php
@@ -3,6 +3,7 @@
 namespace Mctesting\Model\Data;
 
 use Mctesting\Model\Entity\Subcategory;
+use Mctesting\Model\Service\CategoryService;
 use Mctesting\Exception\ApplicationException;
 
 class SubcategoryDAO {
@@ -21,7 +22,6 @@ class SubcategoryDAO {
                 //create object(s) and return
                 $category = \Mctesting\Model\Service\CategoryService::getById($record['catid']);
                 //create  object
-
                 $subcat = new Subcategory();
                 $subcat->setId($record['subcatid']);
                 $subcat->setSubcategory($record['subcatnaam']);
@@ -66,5 +66,36 @@ class SubcategoryDAO {
             throw new ApplicationException('Ophalen subcategorieset statement kan niet worden uitgevoerd');
         }
     }
-
+    
+public static function selectAll() {
+        //create db connection
+        $db = new \PDO(DB_DSN, DB_USER, DB_PASS);
+        //prepare sql statement
+        $sql = 'SELECT * FROM subcategorie';
+        $stmt = $db->prepare($sql);
+        //test if statement can be executed
+        if ($stmt->execute()) {
+            //test if statement retrieved something
+            $resultset = $stmt->fetchall();
+            if (!empty($resultset)) {
+                //create array
+              $subcatarray = array();
+                //create object and return
+                $category = \Mctesting\Model\Service\CategoryService::getById($record['catid']);
+                //create subcategory object(s)
+                foreach ($resultset as $record) {
+                    $subcat = new Subcategory();
+                    $subcat->setId($record['subcatid']);
+                    $subcat->setSubcategory($record['subcatnaam']);
+                    $subcat->setCategory($category);
+                    array_push($subcatarray, $subcat);
+                }
+                return $subcatarray;
+            } else {
+                throw new ApplicationException('Kon subcategorieën niet ophalen, gelieve dit te controleren');
+            }
+        } else {
+            throw new ApplicationException('Ophalen subcategorieën statement kan niet worden uitgevoerd');
+        }
+    }
 }
--- src/Mctesting/Model/Service/SubcategoryService.php
+++ src/Mctesting/Model/Service/SubcategoryService.php
@@ -2,11 +2,12 @@
 
 namespace Mctesting\Model\Service;
 
+use Mctesting\Model\Data\SubcategoryDAO;
 use Mctesting\Model\Data\CategoryDAO;
 
 /***** Author: Bert Mortier *****/
 
-class CategoryService
+class SubcategoryService
 {
     public static function getAll()
     {
@@ -15,6 +16,13 @@ class CategoryService
     
     public static function getById($id)
     {
-        return CategoryDAO::selectById($id);
+        return SubcategoryDAO::selectById($id);
     }
+    
+    public static function getByCategoryId($category)
+    {
+        return SubcategoryDAO::selectByCategoryId($category);
+    }   
+    
+    
 }
