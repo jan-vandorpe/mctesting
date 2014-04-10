31a4cab6545d182925b8a782286a807fd1c66f07
Thomas Deserranno <thomas.deserranno@gmail.com>
2014-04-10 11:16:47 +0200

    fileselecttest modified

--- public/fileselecttest.php
+++ public/fileselecttest.php
@@ -9,7 +9,18 @@
         if ($_POST) {
             ?>
         <pre>
-            <?php print_r($_POST['media']); ?>
+            <?php
+            //assign post values to variables and print variables
+            /*
+             * if no files are selected the post value will not be set so testing
+             * if it is set is needed
+             */
+            $text = $_POST['text'];
+            $media = (isset($_POST['media'])) ? $_POST['media'] : array() ;
+            print ($text);
+            print('<br>');
+            print_r($media);
+            ?>
         </pre>
         
         
@@ -18,6 +29,7 @@
             ?>
         
         <form method="post" action="/mctesting/public/fileselecttest.php">
+            <input type="text" name="text">
             <input type="file" multiple="true" name="media[]">
             <input type="submit">
         </form>
