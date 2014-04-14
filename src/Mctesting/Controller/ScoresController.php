facaab8f342b03c3046b2782c869e7d78362780d
Thomas Deserranno <thomas.deserranno@gmail.com>
2014-04-14 12:53:14 +0200

    scores controller

---
+++ src/Mctesting/Controller/ScoresController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mctesting\Controller;
+
+use Framework\AbstractController;
+
+/**
+ * Description of ScoresController
+ *
+ * @author cyber01
+ */
+class ScoresController extends AbstractController
+{
+    public function selectTest()
+    {
+        //build model
+        //retrieve tests
+        
+        //render page
+        
+    }
+    
+    public function showSessions($testId)
+    {
+        //build model
+        //retrieve test sessions
+        
+        //render page
+    }
+}
