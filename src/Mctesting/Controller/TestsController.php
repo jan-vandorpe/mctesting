934b7e3e8ab26fe5175bee453af5dc7f95123a4d
Bram <brampeters22@hotmail.com>
2014-04-14 11:52:29 +0200

    testsess verder+start testcreate

--- src/Mctesting/Controller/TestsController.php
+++ src/Mctesting/Controller/TestsController.php
@@ -7,11 +7,11 @@ use Mctesting\Model\Service\TestService;
 use Mctesting\Model\Service\UserService;
 
 /**
- * Description of homecontroller
+ * Description of testscontroller
  * 
- * Controller that shows the application homepage.
+ * Controller that handles everything test related
  *
- * @author Bram & Thomas
+ * @author Bram
  */
 class TestsController extends AbstractController
 {
@@ -21,6 +21,9 @@ class TestsController extends AbstractController
     }
     
     public function go()
+            /**
+             * In case of bad redirect, show this
+             */
     {
         //model
         $message1 = 'Landingspagina voor alles ivm de tests (hier zou je normaal niet moeten komen)';
@@ -35,23 +38,75 @@ class TestsController extends AbstractController
         //print_r($_SESSION);
         //var_dump($this->app->getUser());
     }
-    public function testlink()
+    
+    public function testCreation()
+            /**
+             * 
+             */
     {
         //model
-        //$message1 = 'It works!';
         $allTest = TestService::getAll();
         $allUsers = UserService::getAllUsers();
-
         //view
-        $this->render('testlink.html.twig', array(
-          //  'message1' => $message1,
+        $this->render('testcreation.html.twig', array(
             'allTest'=>$allTest,
             'allUsers'=>$allUsers,
-
             ));
-        //print_r($_SESSION);
-        //var_dump($this->app->getUser());
     }
+    
+    public function testlink()
+            /**
+             * 
+             */
+    {
+        //model
+        if(isset($_SESSION["errormsg"])){
+            $message=$_SESSION["errormsg"];
+        }else{
+            $message="";
+        }
+        $allTest = TestService::getAll();
+        $allUsers = UserService::getAllUsers();
+        //view
+        $this->render('testlink.html.twig', array(
+            'allTest'=>$allTest,
+            'allUsers'=>$allUsers,
+            'errormsg'=>$message,
+            ));
+    }
+    
+    
+    public function testlinkadd()
+            /**
+             * Save created session to DB
+             */
+    {
+        
+        $testid=$_POST["testsetselect"];
+        if($testid=="0"){
+            $_SESSION["errormsg"] = "U moet een test selecteren";
+           header("location: /mctesting/tests/testlink");   
+           exit(0);
+        }
+        $datum=$_POST["testdatum"];
+        $sessieww=$_POST["testwachtwoord"];
+        $actief=1;
+        $afgelegd = 0;
+        $users = array();
+        if(isset($_POST["user"])){$users = $_POST["user"];}
+        
+        //print_r($_SESSION);
+        print("<pre>");
+        var_dump($_POST);
+        print("</pre>");
+            if(TestService::insertCreatedTestSessionIntoDB($datum, $testid, $sessieww, $actief, $users,$afgelegd)){
+                //             
+            }else{
+                //niet gelukt
+            }
+        
+    }
+    
 
     
     public function except()
