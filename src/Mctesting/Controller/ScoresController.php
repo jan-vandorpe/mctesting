aa1ec7b124fc2a604085be3dd3177e9b0b8b53eb
Thomas Deserranno <thomas.deserranno@gmail.com>
2014-04-15 12:02:32 +0200

    scores templates done

--- src/Mctesting/Controller/ScoresController.php
+++ src/Mctesting/Controller/ScoresController.php
@@ -17,14 +17,25 @@ class ScoresController extends AbstractController
         //retrieve tests
         
         //render page
+        $this->render('scores_selecttest.html.twig', array());
         
     }
     
     public function showSessions($testId)
     {
         //build model
-        //retrieve test sessions
+        //retrieve all testsessions before today for testId
         
         //render page
+        $this->render('scores_showsessions.html.twig', array());
+    }
+    
+    public function showSessionDetail($testsessionId)
+    {
+        //build model
+        //retrieve testsession 
+        
+        //render page
+        $this->render('scores_showsessiondetail.html.twig', array());
     }
 }
