04466c2e5b8cff4e179f3c3d1484e048fac90961
Bram <brampeters22@hotmail.com>
2014-04-25 10:29:00 +0200

    2504

--- src/Mctesting/Controller/LoginController.php
+++ src/Mctesting/Controller/LoginController.php
@@ -23,12 +23,21 @@ class LoginController extends AbstractController
     public function login()
     {
         $login = $_POST["Login"];
-        $password = $_POST["Wachtwoord"];        
-        if(UserService::loginCheck($login, $password)){
+        $password = $_POST["Wachtwoord"];   
+        if(UserService::isValidEmailFormat($login)){
+          if(UserService::loginCheck($login, $password)){
+            header("location: /mctesting/home/go");        
+        }else{
+            header("location: /mctesting/home/go");        
+        }  
+        }else{
+           if(UserService::loginCheck($login, $password)){
             header("location: /mctesting/choosesession/choosesession");        
         }else{
             header("location: /mctesting/home/go");        
+        }   
         }
+        
         
     }
     
