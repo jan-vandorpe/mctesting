0d696e1ec9d8d79b71c2f21e4063baa36d130c3b
Arne Slabbinck <arne.slabbinck@gmail.com>
2014-09-11 09:29:24 +0200

    voor aanmaken test form cleanup

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,29 +1,47 @@
 $(document).ready(function() {
 
-    $('#frmLogin').bootstrapValidator({
+//    $('#frmLogin').bootstrapValidator({
+//        container: 'tooltip',
+//        feedbackIcons: {
+//            valid: 'glyphicon glyphicon-ok',
+//            invalid: 'glyphicon glyphicon-remove',
+//            validating: 'glyphicon glyphicon-refresh'
+//        },
+//        fields: {
+//            Login: {
+//                message: 'The username is not valid',
+//                validators: {
+//                    notEmpty: {
+//                        message: 'Voornaam is verplicht'
+//                    },
+//                }
+//            },
+//            Wachtwoord: {
+//                message: 'The username is not valid',
+//                validators: {
+//                    notEmpty: {
+//                        message: 'Wachtwoord is verplicht'
+//                    },
+//                }
+//            },
+//        }});
+    $('#testcreation2').bootstrapValidator({
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
             validating: 'glyphicon glyphicon-refresh'
         },
         fields: {
-            Login: {
+            testduration: {
                 message: 'The username is not valid',
                 validators: {
-                    notEmpty: {
-                        message: 'Voornaam is verplicht'
-                    },
-                }
-            },
-            Wachtwoord: {
-                message: 'The username is not valid',
-                validators: {
-                    notEmpty: {
-                        message: 'Wachtwoord is verplicht'
+                    digits: {
+                        message: 'Voer de tijd in minuuten in'
                     },
                 }
             },
         }});
+
 });
 
 
