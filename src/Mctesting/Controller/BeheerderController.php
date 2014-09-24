e5adf3b156701fb8845266ba15622cc52a6b648f
Sam Bourgeois <sam.bourgeois05@hotmail.com>
2014-09-24 16:21:11 +0200

    beheerder toevoegen

--- src/Mctesting/Controller/BeheerderController.php
+++ src/Mctesting/Controller/BeheerderController.php
@@ -16,4 +16,24 @@ class BeheerderController extends AbstractController {
     public function newBeheerderForm(){
         $this->render('newBeheerderform.html.twig', array());
     }
+    
+    public function newBeheerder() {
+        if (isset($_POST["vnaam"]) && isset($_POST["fnaam"]) && isset($_POST["rrnr"]) && isset($_POST["email"]) && isset($_POST["wachtwoord"])) {
+            $user = new User();
+            $user->setRRnr($_POST["rrnr"]);
+            $user->setFirstName($_POST["vnaam"]);
+            $user->setLastName($_POST["fnaam"]);
+            $user->setEmail($_POST["email"]);
+            $user->setGroup("2");
+            $user->setStatus("1");  
+            $wachtwoord = $_POST["wachtwoord"];
+            $timestamp = date('Y-m-d G:i:s');
+            
+            if (true) {
+                header("location: " . ROOT . "/usermanagement/listusers");
+            }
+        } else {
+            throw new ApplicationException('Gelieve alle vakjes in te vullen');
+        }
+    }
 }
