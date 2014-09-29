9297338129f494238a67ba5dc6169f7a5abf93a9
nick-vanpraet <nickvanpraet@gmail.com>
2014-09-29 13:24:47 +0200

    editQuestion twig created, allows users to delete and upload more images for a given question, as well as change everything else or add more answers. Heavy use of jQuery for the images. Minor changes to createQuestion.twig to keep screen from scrolling back up after adding another answer

--- public/js/addanswers.js
+++ public/js/addanswers.js
@@ -11,20 +11,51 @@ $(document).ready(function() {
   });
 
 
-
-
   var $answers = $(".answers"); //antwoorden wrapper
   var $correctAns = $("#correctant"); //correct antw Select box
   var $addButton = $("#AddAnswer"); //Add button ID
+  var $editAddButton = $('#editAddAnswer'); //add button on edit form
 
   //var x               = $antwoorden.length; //aantal initiele anwtoordvelden
-  var count = 2; //minimaal 2 antwoorden aanwezig
+  var count = $(".answers > div").length;
+  console.log(count);
+
+  $editAddButton.click(function(e) {
+    ++count;
+
+    //add input box
+    var strNewAnswer = '<div class="panel panel-default">';
+    strNewAnswer += '<div class="panel-body"><div class="form-group"><label for="antwoord[]" class="col-sm-3 control-label label-padding">Antwoord ' + count + ':</label>';
+    strNewAnswer += '<div class="col-sm-9"><textarea name="antwoord[]" id="antwoord' + count + '" class="form-control allowcode" placeholder="vul hier een antwoord in.">';
+    strNewAnswer += '</textarea></div></div><div class="form-group" id="AnswerMediaBlock' + count + '"><div class="pull-right" id="imgPreview' + count + '" style="padding-right:2em">';
+    strNewAnswer += '<figure class="thumbnail preview-figure btn" onclick="resetInputForm(\'imgPreview' + count + '\', \'AnswerUpload' + count + '\');"><img id="imgPreview' + count + 'img" src="/mctesting/images/placeholder.jpg" class="preview-image thumb">';
+    strNewAnswer += '<span class="preview-close glyphicon glyphicon-remove-circle" title="verwijder afbeelding"></span></figure></div>';
+    strNewAnswer += '<div class="pull-left col-xs-9"><label class="col-xs-4 control-label">Afbeelding:</label><div class="col-xs-8"> <div class="input-group"> <span class="input-group-btn">';
+    strNewAnswer += '<span class="btn btn-default btn-file">Bladeren...<input type="file" name="answerMedia[]" class="form-control uploadImage" ',
+    strNewAnswer += 'onchange="PreviewAnswerImage(\'AnswerUpload' + count + '\', \'imgPreview' + count + '\');" id="AnswerUpload' + count + '"/> </span> </span><input type="text" class="form-control" readonly value="" id="inputText' + count + '"></div></div></div></div>';
+    var $newAnswer = $(strNewAnswer);
+    $answers.append($newAnswer);
+//    console.log($('#antwoord' + count).parent());
+
+    //toevoegen option in correct antwoord select
+    $correctAns.append('<option value="' + (count - 1) + '">Antwoord ' + count + '</option>');
+
+    //herinitialise tinymce op textarea.allowcode
+    tinymce.init({
+      menubar: true,
+      skin: 'bootstrap',
+      toolbar: "",
+      statusbar: true,
+      selector: 'textarea.allowcode',
+      paste_as_text: true,
+      plugins: "visualblocks"
+    });
+  });
+  
 
   $addButton.click(function(e)  //on add input button click
   {
     ++count;
-
-    //add input box
     var strNewAnswer = '<div class="panel panel-default">';
     strNewAnswer += '<div class="panel-body">';
     strNewAnswer += '<div class="form-group">';
@@ -68,7 +99,7 @@ $(document).ready(function() {
 
   });
 
-  
+
 
 }
 );
--- public/js/editQuestions.js
+++ public/js/editQuestions.js
@@ -6,24 +6,97 @@
 
 
 //previewImage function
-function PreviewImage(inputId, previewId) {
-  console.log(inputId, previewId);
+function PreviewImage(inputId, previewId, uploadBox, loop) {
+  console.log(inputId, '#' + previewId, '#' + uploadBox, loop + 1);
+  var idx = loop + 1;
   var oFReader = new FileReader();
   oFReader.readAsDataURL(document.getElementById(inputId).files[0]);
   oFReader.onload = function(oFREvent) {
-    document.getElementById(previewId).src = oFREvent.target.result;
+    $('#' + previewId).append(
+            '<div class="pull-left preview-wrapper" id="imgPreviewNew' + loop + '">\n\
+        <figure class="thumbnail preview-figure btn" onclick="removeImage(\'imgPreviewNew' + loop + '\',\'' + uploadBox + '\')">\n\
+          <img src="' + oFREvent.target.result + '" class="preview-image thumb">\n\
+          <span class="preview-close glyphicon glyphicon-remove-circle" title="verwijder afbeelding"></span>\n\
+        </figure>\n\
+    </div>');
+    $('#' + uploadBox).css("display", "none");
+    $('#inputBox').append(
+            '<div class="form-group" id="UploadBox' + idx + '">\n\
+            <label class="control-label col-xs-3">Afbeelding toevoegen:</label>\n\
+            <div class="col-xs-6">\n\
+              <div class="input-group">\n\
+                <span class="input-group-btn">\n\
+                  <span class="btn btn-default btn-file">\n\
+                    Bladeren... \n\
+                    <input type="file" name="newQMedia[]" class="form-control uploadImage" id="NewQMedia' + idx + '"/>\n\
+                  </span>\n\
+                </span>\n\
+                <input type="text" class="form-control" readonly>\n\
+              </div>\n\
+            </div>\n\
+            <div class="col-xs-3">\n\
+              <input type="button" value="Afbeelding uploaden" id="uploadImgPreview" \n\
+          class="btn btn-default" onclick="PreviewImage(\'NewQMedia' + idx + '\', \'questionImgPreview\', \'UploadBox' + idx + '\',' + idx + ');"></div></div>'
+            );
+    //rerun inputbutton.js
+    $(document).on('change', '.btn-file :file', function() {
+      var input = $(this),
+              numFiles = input.get(0).files ? input.get(0).files.length : 1,
+              label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+      input.trigger('fileselect', [numFiles, label]);
+    });
+
+    $(document).ready(function() {
+      $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+
+        var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if (input.length) {
+          input.val(log);
+        } else {
+          if (log)
+            alert(log);
+        }
+
+      });
+    });
+//    document.getElementById(previewId).src = oFREvent.target.result;
   };
 }
 ;
+
+function PreviewAnswerImage(inputId, previewId) {
+  var oFReader = new FileReader();
+  oFReader.readAsDataURL(document.getElementById(inputId).files[0]);
+  oFReader.onload = function(oFREvent) {
+    document.getElementById(previewId + 'img').src = oFREvent.target.result;
+  };
+}
+
+function removeImage(previewImg, inputIdBlock) {
+  console.log(previewImg, inputIdBlock);
+  $('#' + inputIdBlock).remove();
+  $('#' + previewImg).remove();
+}
+;
+
+function resetInputForm(previewImg, inputId) {
+  $('#' + previewImg + 'img').attr("src", '/mctesting/public/images/placeholder.jpg');
+  $('#' + inputId).wrap('<form>').closest('form').get(0).reset();
+  $('#' + inputId).unwrap();
+  $('#' + inputId).parents('.input-group').find(':text').val('');
+}
 
 //delete img input
 function imgInputDelete(butts) {
   console.log(butts);
   $(butts).empty();
-};
+}
+;
 
 $(document).ready(function() {
-  
+
   var QuestionImgWrapper = $('#questionImgWrapper');
   var AddQuestionPicButton = $('#addQuestionPic');
   var j = 100;
@@ -33,7 +106,7 @@ $(document).ready(function() {
     var strNewQPic = '<div class="form-group">';
     strNewQPic += '<div class="pull-right col-xs-3">';
     strNewQPic += '<figure class="">';
-    strNewQPic += '<img src="/mctesting/public/images/placeholder.jpg" class="img-thumbnail thumb" id="imagePreviewNew'+ j +'"/>';
+    strNewQPic += '<img src="/mctesting/public/images/placeholder.jpg" class="img-thumbnail thumb" id="imagePreviewNew' + j + '"/>';
     strNewQPic += '</figure>';
     strNewQPic += '</div>';
     strNewQPic += '<div class="pull-left col-xs-9">';
@@ -43,7 +116,7 @@ $(document).ready(function() {
     strNewQPic += '<span class="input-group-btn">';
     strNewQPic += '<span class="btn btn-default btn-file">';
     strNewQPic += 'Bladeren... ';
-    strNewQPic += '<input type="file" name="media[]" class="form-control uploadImage" onchange="PreviewImage(\'QuestionUploadNew'+j+'\',\'imagePreviewNew'+ j +'\');" id="QuestionUploadNew'+ j +'"/>';
+    strNewQPic += '<input type="file" name="media[]" class="form-control uploadImage" onchange="PreviewImage(\'QuestionUploadNew' + j + '\',\'imagePreviewNew' + j + '\');" id="QuestionUploadNew' + j + '"/>';
     strNewQPic += '</span>';
     strNewQPic += '</span>';
     strNewQPic += '<input type="text" class="form-control" readonly>';
@@ -53,8 +126,8 @@ $(document).ready(function() {
     strNewQPic += '<div class="pull-left col-xs-9">';
     strNewQPic += '<div class="col-xs-4 control-label"></div>';
     strNewQPic += '<div class="col-xs-8" style="padding-top:0.5em">';
-    strNewQPic += '<label class="btn btn-default" for="ImageId'+ j +'delete">';
-    strNewQPic += '<input type="checkbox" name="ImageId'+ j +'delete" id="ImageId'+ j +'delete">';
+    strNewQPic += '<label class="btn btn-default" for="ImageId' + j + 'delete">';
+    strNewQPic += '<input type="checkbox" name="ImageId' + j + 'delete" id="ImageId' + j + 'delete">';
     strNewQPic += '<span> Afbeelding verwijderen</span>';
     strNewQPic += '</label>';
     strNewQPic += '</div>';
@@ -63,31 +136,31 @@ $(document).ready(function() {
     console.log(j);
     var newQPic = $(strNewQPic);
     QuestionImgWrapper.append(newQPic);
-    
-    
+
+
     //rerun inputbutton.js
-    $(document).on('change', '.btn-file :file', function () {
-        var input = $(this),
-                numFiles = input.get(0).files ? input.get(0).files.length : 1,
-                label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [numFiles, label]);
+    $(document).on('change', '.btn-file :file', function() {
+      var input = $(this),
+              numFiles = input.get(0).files ? input.get(0).files.length : 1,
+              label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+      input.trigger('fileselect', [numFiles, label]);
     });
 
-    $(document).ready(function () {
-        $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+    $(document).ready(function() {
+      $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
 
-            var input = $(this).parents('.input-group').find(':text'),
-                    log = numFiles > 1 ? numFiles + ' files selected' : label;
+        var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
 
-            if (input.length) {
-                input.val(log);
-            } else {
-                if (log)
-                    alert(log);
-            }
+        if (input.length) {
+          input.val(log);
+        } else {
+          if (log)
+            alert(log);
+        }
 
-        });
+      });
     });
   });
-  
+
 });
