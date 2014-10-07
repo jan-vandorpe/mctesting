ce65fa6909782f21372a97287e2bfd50d5d24080
Arne Slabbinck <arne.slabbinck@gmail.com>
2014-10-07 16:25:24 +0200

    fh

--- public/js/myProcessingIcon.js
+++ public/js/myProcessingIcon.js
@@ -2,7 +2,8 @@
 $(document).ready(function () {
 
     //var lad = Ladda.bind('#downloadPDF');
-    $('#downloadPDF').click(function () {
+    $('.downloadPDF').click(function () {
+        $(this).addClass("loading_button");
         var l = Ladda.create(this);
         l.start();
         blockUIForDownload(l);
@@ -12,24 +13,28 @@ $(document).ready(function () {
     function blockUIForDownload(l) {
 
         var token = new Date().getTime(); //use the current timestamp as the token value
-        $('#downloadPDF').attr("href", function (i, href) {
+        $('.loading_button').attr("href", function (i, href) {
             return href + "?downloadId=" + token;
-        })
+        });
 
         fileDownloadCheckTimer = window.setInterval(function () {
 
             var cookieValue = $.cookie("fileDownloadToken");
             console.log($.cookie('fileDownloadToken'));
             if (cookieValue == token)
-                finishDownload(l);
+                finishDownload(l, token);
         }, 500);
     }
 
-    function finishDownload(l) {
+    function finishDownload(l, token) {
         l.stop();
         window.clearInterval(fileDownloadCheckTimer);
         $.removeCookie('fileDownloadToken'); //clears this cookie value
+        $('.loading_button').attr("href", function (i, href) {
 
+            return href.replace("?downloadId=" + token, "");
+        });
+        $('.loading_button').removeClass('loading_button');
     }
 
 
