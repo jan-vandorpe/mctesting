65347c5c1b54ac285d30727c1be14d856ae2e321
Arne Slabbinck <arne.slabbinck@gmail.com>
2014-10-07 11:10:51 +0200

    al keer comitten voor alle aanpassingen

---
+++ public/js/changeColorOnHover.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('.changeColorOnHover').hover(function () {
+
+        $(this).css("background-color", "#F5F5F5");
+    }, function () {
+        $(this).css("background-color", "#FFFFFF");
+    });
+
+
+});
+
+
