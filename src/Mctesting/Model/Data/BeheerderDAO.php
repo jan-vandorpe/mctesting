2a7f89d3d3079cfc4846653adef0eb37d542208a
Sam Bourgeois <sam.bourgeois05@hotmail.com>
2014-10-08 08:48:56 +0200

    beheerder email check

--- src/Mctesting/Model/Data/BeheerderDAO.php
+++ src/Mctesting/Model/Data/BeheerderDAO.php
@@ -54,19 +54,31 @@ class BeheerderDAO {
         //create db connection        
         $db = new \PDO(DB_DSN, DB_USER, DB_PASS);
         //prepare sql statement
-        $sql = 'INSERT INTO `gebruikers`(`rijksregisternr`, `email`, `voornaam`, `familienaam`, `wachtwoord`, `gebruikerstype`, `toegevoegd`) VALUES (:RRNr , :email, :firstName , :lastName , :wachtwoord, :group, :timestamp)';
+        $sql = 'SELECT email FROM gebruikers WHERE email = :email';
         $stmt = $db->prepare($sql);
         //test if statement can be executed
-        if ($stmt->execute(array(':RRNr' => $user->getRRnr(), ':email' => $user->getEmail(), ':firstName' => $user->getFirstName(), ':lastName' => $user->getLastName(), ':wachtwoord' => $password, ':group' => $user->getGroup(), ':timestamp' => $timestamp))) {
-            //test if statement succes
-            return true;
-        } else {
-            $error = $stmt->errorInfo();
-            if ($error[1] == 1062) {
-                throw new ApplicationException('De beheerder (' . $user->getRRnr() . ') bestaat al');
+        if ($stmt->execute(array(':email' => $user->getEmail()))) {
+            $recordset = $stmt->fetchAll();
+            if (empty($recordset)) {
+                $sql = 'INSERT INTO `gebruikers`(`rijksregisternr`, `email`, `voornaam`, `familienaam`, `wachtwoord`, `gebruikerstype`, `toegevoegd`) VALUES (:RRNr , :email, :firstName , :lastName , :wachtwoord, :group, :timestamp)';
+                $stmt = $db->prepare($sql);
+                //test if statement can be executed
+                if ($stmt->execute(array(':RRNr' => $user->getRRnr(), ':email' => $user->getEmail(), ':firstName' => $user->getFirstName(), ':lastName' => $user->getLastName(), ':wachtwoord' => $password, ':group' => $user->getGroup(), ':timestamp' => $timestamp))) {
+                    //test if statement succes
+                    return true;
+                } else {
+                    $error = $stmt->errorInfo();
+                    if ($error[1] == 1062) {
+                        throw new ApplicationException('De beheerder (' . $user->getRRnr() . ') bestaat al. <br> Gelieve een ander rijksregisternummer in te vullen.');
+                    } else {
+                        throw new ApplicationException('Kon geen beheerder in de database invoeren, gelieve dit te controleren:<br>' . $error[2]);
+                    }
+                }
             } else {
-                throw new ApplicationException('Kon geen beheerder in de database invoeren, gelieve dit te controleren:<br>' . $error[2]);
+                throw new ApplicationException('De beheerder (' . $user->getEmail() . ') bestaat al. <br> Gelieve een ander emailadres in te vullen.');
             }
+        } else {
+            throw new ApplicationException('Kon geen beheerder in de database invoeren, gelieve dit te controleren:<br>' . $error[2]);
         }
     }
 
